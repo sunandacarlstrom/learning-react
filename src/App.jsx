819a76783ac87df73b684eae2f9ec3903389218b
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-05-16 13:04:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <section className="container">
+            <h1 className="page-title">Välkommen till Westcoast Cars</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, ut!</p>
+            <label htmlFor="model">Bilmodell</label>
+            <input type="text" name="model" id="model" />
+        </section>
+    );
+}
+
+export default App;
