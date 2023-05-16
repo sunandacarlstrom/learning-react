ee8da48a2282826dbceb2837fe6a2836f537b410
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-05-16 17:20:43 +0200

    Lagt till standardvärde för headerText

--- src/components/ui/PageHeader.jsx
+++ src/components/ui/PageHeader.jsx
@@ -1,5 +1,17 @@
+import PropTypes from "prop-types";
+
 const PageHeader = ({headerText}) => {
     return <h1>{headerText}</h1>
 }; 
+
+// Definiera egenskapers typer och om de är obligatoriska
+PageHeader.propTypes = {
+    headerText: PropTypes.string
+}; 
+
+// Standardvärde på egenskaper
+PageHeader.defaultProps = {
+    headerText: "Rubrik för sidan"
+};  
 
 export default PageHeader; 
